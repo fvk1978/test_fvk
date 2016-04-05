eab1db9ac22c1bb42c8eb3368e330fdeda7ab5c8
Fedor Kobzarenko <fedorvk@gmail.com>
2016-04-05 21:47:57 +0800

    trying to fix wsgi error

--- test_fvk/wsgi.py
+++ test_fvk/wsgi.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_fvk.settings")
+import django
+django.setup()
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
